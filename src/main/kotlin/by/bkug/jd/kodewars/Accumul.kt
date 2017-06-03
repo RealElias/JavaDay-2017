f27f3f5a2df013a9266397ac5b2f5fc1000a9561
realyelias <realyelias@gmail.com>
2017-06-03 03:09:46 +0300

    task1

--- src/main/kotlin/by/bkug/jd/kodewars/Accumul.kt
+++ src/main/kotlin/by/bkug/jd/kodewars/Accumul.kt
@@ -1,5 +1,7 @@
 package by.bkug.jd.kodewars
 
+import java.util.stream.Collectors
+
 /**
  * This time no story, no theory. The examples below show you how to write function accum:
  * Examples:
@@ -11,5 +13,11 @@ package by.bkug.jd.kodewars
  * The parameter of accum is a string which includes only letters from a..z and A..Z.
  */
 fun accum(s: String): String {
-    TODO("1 Point")
+    var counter : Int = 0
+
+    return s.split("").stream()
+            .filter(String::isNotBlank)
+            .map(String::toLowerCase)
+            .map { c -> c.toUpperCase() + 1.rangeTo(counter++).map { c }.stream().collect(Collectors.joining())  }
+            .collect(Collectors.joining("-"))
 }
